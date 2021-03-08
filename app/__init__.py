e11b0c62b2fba430c6f05c3e9e9b969c9f6263b4
Kizito-Alberrt <wagabi98@gmail.com>
2021-03-08 21:50:30 +0300

    included forms.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,41 @@
 from flask import Flask
+from flask_bootstrap import Bootstrap
+from config import config_options
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_mail import Mail
 
+# Initializing Login manager
+login_manager = LoginManager()
+login_manager.session_protection = 'storng'
+login_manager.login_view = 'auth.login'
 
-app = Flask(__name__)
+# Initializing Extensions
+bootstrap = Bootstrap()
+db = SQLAlchemy()
+mail = Mail()
+
+def create_app(config_name):
+
+    app = Flask(__name__)
+
+    # Creating the app configuration
+    app.config.from_object(config_options[config_name])
+
+    # Initalizing flask extensions
+    bootstrap.init_app(app)
+    db.init_app(app)
+    mail.init_app(app)
+    login_manager.init_app(app)
+
+    # Registering the BLUEPRINT
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint, url_prefix = '/auth')
+
+    from .requests import configure_request
+    configure_request(app)
+
+    return app
